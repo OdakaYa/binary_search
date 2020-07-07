7982f97a8e828e4da3165e213a57317e2fc43e3f
Odaka Yasuaki <odaka.y.ac@m.titech.ac.jp>
2020-07-07 11:44:35 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,17 @@ int n;
 int k;
 int A[100000];
 
+int need_bag(int m){
+  int nb = 0;
+  for(int i = 0; i < n; i ++){
+    nb = nb + (A[i] + m - 1) / m;
+  }
+  return nb;
+}
+
+int p(int m){
+  return need_bag(m) <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +22,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 1e5;
+  while(ub - lb > 1) {
+    int mid = (lb + ub) / 2;
+    if(p(mid)){
+      ub = mid;
+    }
+    else {
+      lb = mid;
+    }
+  }
+  printf("%d\n", ub);
   return 0;
 }
